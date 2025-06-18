64ab75bb26e6a1b7f4e5b64e3dcd228aef66383d
Dinesh Upadhyay <84572769+dineshupadhyay08@users.noreply.github.com>
2025-06-18 10:39:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "../components/hero";
-import Experience from "../components/experience";
 import Education from "../components/education";
 import Skills from "../components/skills";
 import Projects from "../components/projects";
