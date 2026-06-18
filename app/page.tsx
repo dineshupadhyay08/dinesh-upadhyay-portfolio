deca283bc9cd343960cd0a5fc4f15d5facc8cb61
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-06-18 23:28:26 +0530

    Improve Portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ import Education from "../components/education";
 import Skills from "../components/skills";
 import Projects from "../components/projects";
 import Contact from "../components/contact";
+import Experience from "@/components/experience";
 
 export default function Home() {
   return (
     <div className="mx-auto max-w-5xl space-y-20 py-10">
       <Hero />
-      {/* <Experience /> */}
+      <Experience />
       <Education />
       <Skills />
       <Projects />
